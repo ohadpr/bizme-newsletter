fd157e1b044803f72f841dbe5306dfe660bd0d73
Ohad Eder-Pressman <ohad@visual-i.com>
2017-10-15 12:06:22 +0300

    Trying to mustache an array of strings

--- admin/admin.js
+++ admin/admin.js
@@ -6,13 +6,9 @@ var PostPreview = createClass({
     var entry = this.props.entry;
     var data = entry.get('data').toJS();
 
-    console.log(data['closing']);
-    
     // convert the 'closing' field from a string to an array of strings based on line breaks
     data['closing'] = data['closing'].split(/[\r\n]+/)
                         .map(function(line) { return { line: line } });
-
-    console.log(data['closing']);
 
     var html = mustache.render(template, data);
     return h('div', {"dangerouslySetInnerHTML": {"__html":html}});
