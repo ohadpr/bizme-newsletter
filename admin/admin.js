009e70823214ddea34594107eb4a759f9a6913d4
Ohad Eder-Pressman <ohad@visual-i.com>
2017-10-15 12:02:04 +0300

    test multiline closing

--- admin/admin.js
+++ admin/admin.js
@@ -5,6 +5,15 @@ var PostPreview = createClass({
   render: function() {
     var entry = this.props.entry;
     var data = entry.get('data').toJS();
+
+    console.log(data['closing']);
+    
+    // convert the 'closing' field from a string to an array of strings based on line breaks
+    data['closing'] = data['closing'].split(/[\r\n]+/)
+                        .map(function(line) { return { line: line } });
+
+    console.log(data['closing']);
+
     var html = mustache.render(template, data);
     return h('div', {"dangerouslySetInnerHTML": {"__html":html}});
   }
